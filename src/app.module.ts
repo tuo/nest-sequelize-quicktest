f22e9b46fa3341eaab2fc7233b6fb293a6f77f38
Tuo <tuo.huang@6e-group.com>
2020-10-15 17:17:35 +0800

    added basic sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,17 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'nest_blog_api',
-      autoLoadModels: true,
-      synchronize: false,
+    SequelizeModule.forRootAsync({
+      useFactory: () => ({
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: '123456',
+        database: 'nest_blog_api',
+        autoLoadModels: true,
+        synchronize: false,
+      }),
     }),
     UsersModule,
   ],
